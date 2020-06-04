2abdcb45313d5da8dc23c4249c06a121afabc1b7
Tomasz Szymanik <tmszymanik@gmail.com>
2020-06-04 21:36:08 +0200

    Build v2

--- kgrzybowski/component---src-pages-test-index-tsx-c68d1e4a36f60dbff648.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{JdD0:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("lkFK"),r=n("H8eV");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Test"}),l.a.createElement("h1",null,"Test"))}}}]);
-//# sourceMappingURL=component---src-pages-test-index-tsx-c68d1e4a36f60dbff648.js.map
---
+++ kgrzybowski/component---src-pages-test-index-tsx-de1ce99dbb98aac95529.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{JdD0:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("lkFK"),r=n("H8eV");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Test"}),l.a.createElement("h1",null,"Test"))}}}]);
+//# sourceMappingURL=component---src-pages-test-index-tsx-de1ce99dbb98aac95529.js.map
